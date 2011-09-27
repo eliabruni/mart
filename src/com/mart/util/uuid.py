e712b3e642684cd462171e36827956f6a9385500
Elia Bruni <elia.bruni@gmail.com>
2011-09-27 12:30:03 +0200

    uuid.py refactoring

--- src/com/mart/util/uuid.py
+++ src/com/mart/util/uuid.py
@@ -1,20 +1,18 @@
-import glob, os, shutil, uuid, sys
+import random
+import glob, os, shutil, sys
 
 
-def assign_uuid_to_files(dataset_path):
-
+def assign_uuid_to_files(dataset_path, label_file):
     uuid_path = os.path.join(dataset_path, 'uuid')
     os.mkdir(uuid_path)
     os.mkdir(os.path.join(uuid_path, 'labels'))
     os.mkdir(os.path.join(uuid_path, 'images'))
-
-    num_files = len([f for f in os.listdir(os.path.join(dataset_path, 'original_data/images'))]
-
-    file_scores = {}
+    num_files = len([f for f in os.listdir(os.path.join(dataset_path, 'original-dataset/images'))])
+    print num_files
+    file_score = {}
     UUIDS = []
-
     for ii in range(0, num_files):
-        UUIDS.append(uuid.uuid1())
+        UUIDS.append(get_random_string(32))
     UUIDS.sort()
 
     file = open(label_file, 'r')
@@ -29,11 +27,11 @@ def assign_uuid_to_files(dataset_path):
     file.close()
 
     count = 0
-    for name in file_scores:
+    for name in file_score:
         new_label_name     = str(UUIDS[count]) + '.txt'
         new_image_name     = str(UUIDS[count]) + '.jpg'
         new_label_path     = uuid_path + '/labels/' + new_label_name
-        old_image_path     = dataset_path + 'original-dataset/images/' + name
+        old_image_path     = dataset_path + '/original-dataset/images/' + name
         new_image_path     = uuid_path + '/images/' + new_image_name
         label_file         = open(new_label_path, 'w')
         label_file.write(file_score[name][0] + ' ' + file_score[name][1])
@@ -41,9 +39,17 @@ def assign_uuid_to_files(dataset_path):
         shutil.copy(str(old_image_path), new_image_path)
         count += 1
 
+def get_random_string(word_len):
+    word = ''
+    for i in range(word_len):
+        word += random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
+    return word
+
 if __name__ =="__main__":
     _dataset_path = sys.argv[1]
-    assign_uuid_to_files(_dataset_path)
+    _label_file   = sys.argv[2]
+    
+    assign_uuid_to_files(_dataset_path, _label_file)
     
 
     
